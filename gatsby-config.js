3974f52eb6c26300c38d68070b7998f7393bb2df
Nicolas <info@nicolas-moyano.se>
2019-10-28 16:54:04 +0100

    my first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Nicolas Moyano`,
+    description: `A Digital Art director & Product Designer With Focus On User Experience`,
     author: `@gatsbyjs`,
   },
   plugins: [

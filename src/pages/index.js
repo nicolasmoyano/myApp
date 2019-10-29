3ad986e12a7e05baff7a87d6518eaa28f933f3c5
Nicolas <info@nicolas-moyano.se>
2019-10-29 11:21:52 +0100

    changed theme color in config-js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div style={{ marginBottom: `1.45rem` }}>
-    <h1 style={{ fontSize: `5.8rem`, lineHeight: '4.8rem', textAlign: `center`, maxWidth: 1200, margin: '0 auto', 
-verticalAlign: 'middle', padding: '25% 0'}}>Digital Art Direction <span style={{color: '#62EBFF'}}>&</span> Product design.</h1>
+    <h1 style={{ fontSize: `5.8rem`,
+    lineHeight: '4.8rem',
+    textAlign: `center`,
+    maxWidth: 1200, margin: '0 auto', 
+    verticalAlign: 'middle',
+    padding: '25% 0'}}>Digital Art Direction <span style={{color: '#62EBFF'}}>&</span> Product design.</h1>
+    </div>
+
+    <div style={{ margin: '0 auto', verticalAlign: 'center' }}>
+      <span >Scroll Down</span>
     </div>
   </Layout>
 )

608545adc36e8f126af2411f01a01ff6c65b38aa
Nicolas <info@nicolas-moyano.se>
2019-10-29 10:20:46 +0100

    changed header color and font changes

--- src/components/header.js
+++ src/components/header.js
@@ -5,22 +5,27 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `white`,
       marginBottom: `1.45rem`,
+      borderBottom: '1px solid',
+      borderColor: '#e0e0e0'
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: '100vw',
+        padding: `1.4rem 4.5rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            fontFamily: `Open Sans`,
+            fontSize: '1.5rem',
+            fontWeight: 900,
+            color: `#505050`,
             textDecoration: `none`,
           }}
         >
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Digital Art Direction & Product design.</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ maxWidth: 1200, marginBottom: `1.45rem` }}>
+    <h1 style={{ fontSize: `5rem`, textAlign: `center`}}>Digital Art Direction & Product design.</h1>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
